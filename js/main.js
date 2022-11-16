1dd63ab8a8139be9dfe48e5c05153450df77f366
Krypto1337 <118254306+Krypto1337@users.noreply.github.com>
2022-11-16 14:14:28 +0100

    Added variables

--- js/main.js
+++ js/main.js
@@ -14,3 +14,14 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+//Variables
+let numb1 = 0;
+let numb2 = 0;
+let result = 0;
+let decision = "";
+
+const currentScreen = document.querySelector(".screen-current");
+const lastScreen = document.querySelector(".screen-last");
+const buttons = document.querySelectorAll(".btn");
+const clear = document.querySelector(".btn-clear");
