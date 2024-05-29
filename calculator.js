d5afae129a6426853f17c2d4b692aa179b5f0bc7
Rene B <133756710+Krypto1337@users.noreply.github.com>
2024-05-29 15:53:45 +0200

    Added divide-function which takes an array as input and return the quotient

--- calculator.js
+++ calculator.js
@@ -19,6 +19,14 @@ function multiply(input) {
 	return product;
 }
 
+function divide(input) {
+	const quotient = input.reduce((total, currentNumber) => {
+		return total / currentNumber;
+	});
+	return quotient;
+}
+
 console.log(add([1, 2, 3, 4, 5]));
 console.log(subtract([55, 6, 7]));
 console.log(multiply([10, 2, 5]));
+console.log(divide([55, 11, 5]));
