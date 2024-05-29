3e2fb22004d7623328703c80eb94dba50fddbe45
Rene B <133756710+Krypto1337@users.noreply.github.com>
2024-05-29 15:37:53 +0200

    Added subtract-function which takes an array of numbers and return the difference

--- calculator.js
+++ calculator.js
@@ -5,4 +5,12 @@ function add(input) {
 	return sum;
 }
 
+function subtract(input) {
+	const difference = input.reduce((total, currentNumber) => {
+		return total - currentNumber;
+	});
+	return difference;
+}
+
 console.log(add([1, 2, 3, 4, 5]));
+console.log(subtract([55, 6, 7]));
